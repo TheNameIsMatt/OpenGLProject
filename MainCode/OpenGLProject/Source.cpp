d4fcc068863df246d132044b8bfff56ebfba452e
ImPukes <matthew.rigz@outlook.com>
2022-04-12 17:01:03 +0100

    Fixed issue with framebuffer callback not working correctly. Didnt realise that methods needed to be written procedurally 🥴

--- MainCode/OpenGLProject/Source.cpp
+++ MainCode/OpenGLProject/Source.cpp
@@ -1,5 +1,17 @@
-#include <glad/glad.h>
+#include <Windows.h>
+#include <glad/glad.h>	
+#include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include "Source.h"
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 
 int main()
 {
@@ -8,5 +20,47 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+
+	// We pass GLAD the function to load the address of the OpenGL function pointers which is OS-specific. GLFW gives us glfwGetProcAddress that defines the correct function based on which OS we're compiling for.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Failed to initialise GLAD" << std::endl;
+		return -1;
+	}
+	// The first two parameters of glViewport set the location of the lower left corner of the window. The third and fourth parameter set the width and height of the rendering window in pixels, which we set equal to GLFW's window size.
+	// OpenGL starts in the lower left wherea DirectX starts in the top left.
+	// 
+	// Behind the scenes OpenGL uses the data specified via glViewport to transform the 2D coordinates it processed to coordinates on your screen. For example, a processed point of location (-0.5,0.5) would (as its final transformation) be mapped to (200,450) 
+	// in screen coordinates. Note that processed coordinates in OpenGL are between -1 and 1 so we effectively map from the range (-1 to 1) to (0, 800) and (0, 600).
+	glViewport(0, 0, 800, 600);
+
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+	//Double buffer
+	// When an application draws in a single buffer the resulting image may display flickering issues.This is because the resulting output image is not drawn in an instant, 
+	 // but drawn pixel by pixeland usually from left to rightand top to bottom.Because this image is not displayed at an instant to the user while still being rendered to, 
+	// the result may contain artifacts.To circumvent these issues, windowing applications apply a double buffer for rendering.The front buffer contains the final output image that is shown at the screen, 
+	// while all the rendering commands draw to the back buffer.As soon as all the rendering commands are finished we swap the back buffer to the front buffer so the image can be displayed without still being rendered to, removing all the aforementioned artifacts.
+	while (!glfwWindowShouldClose(window)) 
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+
+
 }
---
+++ MainCode/OpenGLProject/Source.h
@@ -0,0 +1,11 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+class Source {
+
+public: 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+};
